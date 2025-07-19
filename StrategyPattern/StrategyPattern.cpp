546c131cfc33dd912f698276227dd8c93d551d23
FURKAN <furkanozdemir199998@gmail.com>
2025-07-19 18:42:58 +0300

    Add context class

--- StrategyPattern/StrategyPattern.cpp
+++ StrategyPattern/StrategyPattern.cpp
@@ -9,5 +9,15 @@ void ClientCode(INavigation& navigation) {
 int main()
 {
     std::unique_ptr<StrategyFactory> factory = std::make_unique<StrategyFactory>();
-    ClientCode(*(factory->create(NavigatorType::kBicycle)));
+    ClientCode(*(factory->create(NavigatorType::kBicycle))); // one of the way is create a factory class for manage the creation of neccesary strategy object
+    
+    //second way is create and use a context class which set and get neccesary strategies with a method in runtime and call the core logic in it via the selected strategy core logic in it
+    std::unique_ptr<context> set_strategy = std::make_unique<context>(std::make_unique<Car>()); // we set the strategy to the car
+    set_strategy->doSomeBusinessLogic(); // it will call the bicycle one because we give reference of bicycle object.
+
+    //now change the strategy with set method ->
+    set_strategy->SetStrategy(std::make_unique<Bus>());
+    set_strategy->doSomeBusinessLogic();
+
+    //or use a factory class like above for control it from one main section.
 }
--- StrategyPattern/StrategyPattern.h
+++ StrategyPattern/StrategyPattern.h
@@ -14,23 +14,52 @@ public:
     virtual void differentTypesOfNavigator() = 0;
 };
 
+class context {
+public:
+    context(std::unique_ptr<INavigation> navigation) : navigation_(std::move(navigation)) {
+
+    }
+
+    void SetStrategy(std::unique_ptr<INavigation> navigation) {
+        navigation_ = std::move(navigation);
+    }
+
+    std::unique_ptr<INavigation> GetStrategy() {
+        return std::move(navigation_);
+    }
+
+    void doSomeBusinessLogic() const
+    {
+        if (navigation_) {
+            std::cout << "find a shortest way to go with the strategy (not sure how it'll do it)\n";
+            navigation_->differentTypesOfNavigator();
+        }
+        else {
+            std::cout << "Context: Strategy isn't set\n";
+        }
+    }
+
+private:
+    std::unique_ptr<INavigation> navigation_;
+};
+
 class Bicycle : public INavigation {
 public:
     void differentTypesOfNavigator() override {
-        std::cout << "show shortest way to selected destination via bicycle!!";
+        std::cout << "show shortest way to selected destination via bicycle!!\n";
     }
 };
 
 class Bus : public INavigation {
 public:
     void differentTypesOfNavigator() override {
-        std::cout << "show shortest way to selected destination via bus!!";
+        std::cout << "show shortest way to selected destination via bus!!\n";
     }
 };
 
 class Car : public INavigation {
 public:
     void differentTypesOfNavigator() override {
-        std::cout << "show shortest way to selected destination via car!!";
+        std::cout << "show shortest way to selected destination via car!!\n";
     }
 };
